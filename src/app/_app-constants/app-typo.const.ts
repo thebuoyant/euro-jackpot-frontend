e8ef165b6f8cdb7ffd0327cd261c8012a9a9888d
Thomas Schlender <thebuoyant@gmail.com>
2025-10-05 22:11:06 +0200

    EJF-10 Improve dashboard page

---
+++ src/app/_app-constants/app-typo.const.ts
@@ -0,0 +1,7 @@
+export const APP_TYPO_CONST = {
+  pages: {
+    dashboard: {
+      headerTitle: "Dashboard",
+    },
+  },
+};
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,14 @@
+import { Typography } from "@mui/material";
 import "./Dashboard.css";
+import { APP_TYPO_CONST } from "../_app-constants/app-typo.const";
 
 export default function DashboardPage() {
   return (
     <div className="dashboard-page">
       <div className="dashboard-page-header page-header">
-        dashboard page header
+        <Typography variant="h6">
+          {APP_TYPO_CONST.pages.dashboard.headerTitle}
+        </Typography>
       </div>
       <div className="dashboard-page-content page-content">
         dashboard page content
