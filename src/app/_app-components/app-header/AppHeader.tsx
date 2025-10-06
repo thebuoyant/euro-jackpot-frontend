03af1c1bee43ccd84ed7bb37efc12e6daf1f02e8
Thomas Schlender <thebuoyant@gmail.com>
2025-10-06 17:46:47 +0200

    EJF-11 Improve sidebar navigation

--- src/app/_app-components/app-header/AppHeader.tsx
+++ src/app/_app-components/app-header/AppHeader.tsx
@@ -2,15 +2,25 @@ import { APP_COLOR_CONST } from "src/app/_app-constants/app-color.const";
 import "./AppHeader.css";
 import { Typography } from "@mui/material";
 import { APP_CONST } from "src/app/_app-constants/app.const";
+import { useRouter } from "next/navigation";
+import { APP_ROUTE_CONST } from "src/app/_app-constants/app-route.const";
 
 export default function AppHeader() {
+  const router = useRouter();
+
+  const handleNavigationToHome = () => {
+    router.push(APP_ROUTE_CONST.home, { scroll: true });
+  };
+
   return (
     <div
       className="app-header"
       style={{
         backgroundColor: APP_COLOR_CONST.colorPrimary,
         color: APP_COLOR_CONST.colorWhite,
+        cursor: "pointer",
       }}
+      onClick={handleNavigationToHome}
     >
       <Typography variant="h6">{APP_CONST.appTitle}</Typography>
     </div>
--- src/app/_app-components/app-sidebar/AppSidebar.tsx
+++ src/app/_app-components/app-sidebar/AppSidebar.tsx
@@ -1,18 +1,35 @@
 import { APP_TYPO_CONST } from "src/app/_app-constants/app-typo.const";
 import NavigationItem from "../_static/navigation-item/NavigationItem";
 import "./AppSidebar.css";
+import { usePathname, useRouter } from "next/navigation";
+import { APP_ROUTE_CONST } from "src/app/_app-constants/app-route.const";
 
 export default function AppSidebar() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleNavigationToDashboard = () => {
+    router.push(APP_ROUTE_CONST.dashboard, { scroll: true });
+  };
+
+  const handleNavigationToArchive = () => {
+    router.push(APP_ROUTE_CONST.archive, { scroll: true });
+  };
+
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-content">
         <NavigationItem
           title={APP_TYPO_CONST.sidebar.navItemDashboard}
           iconType="dashboard"
+          isActive={pathname?.includes("dashboard")}
+          onClick={handleNavigationToDashboard}
         />
         <NavigationItem
           title={APP_TYPO_CONST.sidebar.navItemArchive}
           iconType="archive"
+          isActive={pathname?.includes("archive")}
+          onClick={handleNavigationToArchive}
         />
       </div>
     </div>
