fa2336990d0f73a5e6454b227d7142853d9520b3
Thomas Schlender <thebuoyant@gmail.com>
2025-10-08 21:12:19 +0200

    EJF-25 Add base dashboard card for first draw

---
+++ src/app/_app-components/_static/dashboard-card-first-draw/DashboardCardFirstDrawCard.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import React from "react";
+import { Card, CardContent, Divider, Typography } from "@mui/material";
+import { DrawRecord } from "src/app/_app-types/record.types";
+import {
+  formatNumberToString,
+  resolveDay,
+} from "src/app/_app-utils/record.util";
+
+export default function DashboardCardFirstDraw({
+  title,
+  labelDate,
+  labelWinningNumbers,
+  labelEuroNumbers,
+  labelStake,
+  labelDay,
+  draw,
+}: {
+  title: string;
+  labelDate: string;
+  labelWinningNumbers: string;
+  labelEuroNumbers: string;
+  labelStake: string;
+  labelDay: string;
+  draw: DrawRecord;
+}) {
+  if (!draw) {
+    return null;
+  }
+
+  return (
+    <Card className="card" elevation={4}>
+      <CardContent>
+        <Typography variant="h6" gutterBottom>
+          {title}
+        </Typography>
+
+        <Divider sx={{ my: 2 }} />
+
+        <ul className="card-list">
+          <li>
+            <span className="label">{`${labelDate}:`}</span>
+            <span className="value">{draw.datum}</span>
+          </li>
+          <li>
+            <span className="label">{`${labelWinningNumbers}:`}</span>
+            <span className="value">{`${draw.nummer1} | ${draw.nummer2} | ${draw.nummer3} | ${draw.nummer4} | ${draw.nummer5}`}</span>
+          </li>
+          <li>
+            <span className="label">{`${labelEuroNumbers}:`}</span>
+            <span className="value">{`${draw.zz1} | ${draw.zz2}`}</span>
+          </li>
+          <li>
+            <span className="label">{`${labelStake}:`}</span>
+            <span className="value">{`${formatNumberToString(
+              draw.spielEinsatz,
+              2
+            )} €`}</span>
+          </li>
+          <li>
+            <span className="label">{`${labelDay}:`}</span>
+            <span className="value">{`${resolveDay(draw.tag)}`}</span>
+          </li>
+        </ul>
+      </CardContent>
+    </Card>
+  );
+}
