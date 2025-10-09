fbc5cfe60089f1a5c510fe92dce65da758d359e0
Thomas Schlender <thebuoyant@gmail.com>
2025-10-09 19:35:16 +0200

    EJB-30 Improve app start to refer to dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import { Typography } from "@mui/material";
 import { APP_TYPO_CONST } from "./_app-constants/app-typo.const";
+import { useEffect } from "react";
+import { APP_ROUTE_CONST } from "./_app-constants/app-route.const";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(APP_ROUTE_CONST.dashboard, { scroll: true });
+  }, [router]);
+
   return (
     <div className="home-page">
       <div className="home-page-header page-header">
@@ -9,9 +20,7 @@ export default function Home() {
           {APP_TYPO_CONST.pages.home.headerTitle}
         </Typography>
       </div>
-      <div className="home-page-content page-content">
-        home page content
-      </div>
+      <div className="home-page-content page-content">home page content</div>
     </div>
   );
 }
